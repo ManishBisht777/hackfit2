f77647fe8f8484cf46e76516129666520bbee98c
manish bisht <manish205770@keshav.du.ac.in>
2023-01-22 01:53:35 +0530

    Feat: meta data to pages

--- pages/exercise.tsx
+++ pages/exercise.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { firestore } from "@/firebase/firebase";
 import Part from "@/components/part";
+import Head from "next/head";
 
 export default function Exercise() {
   const [exercise, setExercise] = useState<any>([]);
@@ -20,11 +21,19 @@ export default function Exercise() {
   }, []);
 
   return (
-    <main className="p-[3rem] pt-[7rem]">
-      {exercise &&
-        exercise.map((exercise: any, index: number) => {
-          return <Part key={index} exercise={exercise.exercise} />;
-        })}
-    </main>
+    <>
+      <Head>
+        <title>Exercise</title>
+        <meta name="description" content="Exercise Page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="p-[3rem] pt-[7rem]">
+        {exercise &&
+          exercise.map((exercise: any, index: number) => {
+            return <Part key={index} exercise={exercise.exercise} />;
+          })}
+      </main>
+    </>
   );
 }
